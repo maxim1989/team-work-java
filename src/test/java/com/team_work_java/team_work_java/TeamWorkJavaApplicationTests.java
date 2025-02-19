28944a25521e6ea49f4133dad552e23ec0f37fe6
maksimfedorov <fedorov-max89@mail.ru>
2025-02-19 20:42:02 +0300

    [#1]: развернут базовый проект

---
+++ src/test/java/com/team_work_java/team_work_java/TeamWorkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_work_java.team_work_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
