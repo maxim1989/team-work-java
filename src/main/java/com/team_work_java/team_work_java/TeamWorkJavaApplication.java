77d62fed125f97fd6e6539ed8a26b8f1bbf0763b
maksimfedorov <fedorov-max89@mail.ru>
2025-02-23 19:05:06 +0300

    [#4]: RecommendationsController

--- src/main/java/com/team_work_java/team_work_java/TeamWorkJavaApplication.java
+++ src/main/java/com/team_work_java/team_work_java/TeamWorkJavaApplication.java
@@ -1,13 +1,15 @@
 package com.team_work_java.team_work_java;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class TeamWorkJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeamWorkJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeamWorkJavaApplication.class, args);
+    }
 
 }
--- src/main/java/com/team_work_java/team_work_java/service/RecommendationsServiceImpl.java
+++ src/main/java/com/team_work_java/team_work_java/service/RecommendationsServiceImpl.java
@@ -1,10 +1,12 @@
 package com.team_work_java.team_work_java.service;
 
 import com.team_work_java.team_work_java.dto.RecommendationDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class RecommendationsServiceImpl implements RecommendationsService {
     @Override
     public List<RecommendationDto> getRecommendationsByUserId(UUID id) {
