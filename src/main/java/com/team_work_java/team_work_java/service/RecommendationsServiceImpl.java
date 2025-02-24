eaeb443a027e10221a96b93b600ad5a6eecf703a
maksimfedorov <fedorov-max89@mail.ru>
2025-02-24 20:42:41 +0300

    [#13]: перебирать рекоммендации в цикле

--- src/main/java/com/team_work_java/team_work_java/service/RecommendationsServiceImpl.java
+++ src/main/java/com/team_work_java/team_work_java/service/RecommendationsServiceImpl.java
@@ -19,27 +19,14 @@ public class RecommendationsServiceImpl implements RecommendationsService {
     @Override
     public List<Optional<RecommendationDto>> getRecommendationsByUserId(UUID id) {
         final List<Optional<RecommendationDto>> recommendationDtoList = new ArrayList<>();
-        final Optional<RecommendationDto> recommendationRuleSetInvest500Impl =
-                recommendationRuleSetMap.get("recommendationRuleSetInvest500Impl").getRecommendations(id);
 
-        if (recommendationRuleSetInvest500Impl.isPresent()) {
-            recommendationDtoList.add(recommendationRuleSetInvest500Impl);
-        }
-
-        final Optional<RecommendationDto> recommendationRuleSetSimpleCreditImpl =
-                recommendationRuleSetMap.get("recommendationRuleSetSimpleCreditImpl").getRecommendations(id);
-
-        if (recommendationRuleSetSimpleCreditImpl.isPresent()) {
-            recommendationDtoList.add(recommendationRuleSetSimpleCreditImpl);
-        }
-
-        final Optional<RecommendationDto> recommendationRuleSetTopSavingImpl =
-                recommendationRuleSetMap.get("recommendationRuleSetTopSavingImpl").getRecommendations(id);
-
-        if (recommendationRuleSetTopSavingImpl.isPresent()) {
-            recommendationDtoList.add(recommendationRuleSetTopSavingImpl);
-        }
-
+        recommendationRuleSetMap.forEach((k, v) -> {
+            final Optional<RecommendationDto> recommendation =
+                    recommendationRuleSetMap.get(k).getRecommendations(id);
+            if (recommendation.isPresent()) {
+                recommendationDtoList.add(recommendation);
+            }
+        });
         return recommendationDtoList;
     }
 }
